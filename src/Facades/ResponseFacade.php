6359b7acee9b09a9b3d84739895a61d9f7364d0e
Hoang Nguyen <hoangnguyen@vietswiss.com>
2022-04-04 17:38:03 +0700

    update document

--- src/Facades/ResponseFacade.php
+++ src/Facades/ResponseFacade.php
@@ -3,7 +3,11 @@
 namespace Max26292\LaravelLazyTools\Facades;
 
 use Illuminate\Support\Facades\Facade;
-
+use Illuminate\Http\JsonResponse;
+/**
+ * @method static JsonResponse responseSuccess(int $statusCode, array $data = [], array $messages = [])
+ * @method static JsonResponse responseError(int $statusCode, array $messages = [], array $data = [], $errorCode = 'ERR0')
+ */
 class ResponseFacade extends Facade
 {
     protected static function getFacadeAccessor()
